06e274b7211f4e44b25ca376907720854c996d86
nawab-mac <bondkn@gmail.com>
2023-02-23 00:26:57 +0530

    javascript file added

---
+++ jqueryforhighlighter.js
@@ -0,0 +1,52 @@
+//fetch ids of books
+//class id for every book title form main page a-row.kp-notebook-library-each-book
+var ids = new Array();
+$('.a-row.kp-notebook-library-each-book').each(function() {
+currentid= $(this).attr("id"); //Get elements that have an id=
+  ids.push(currentid);console.log(currentid); //add id to array
+  fetch('https://read.amazon.com/notebook?asin='+ currentid + '&contentLimitState=&').then(function (response) {
+	// The API call was successful!
+	return response.text();
+}).then(function (html) {
+
+	// Convert the HTML string into a document object
+	var parser = new DOMParser();
+	var doc = parser.parseFromString(html, 'text/html');
+	console.log(html);
+	console.log(doc);
+
+	  //id = doc.getElementsById("annotationNoteHeader")[0];
+	  //console.log(id);
+
+
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});
+
+
+});
+
+//call amazon and get all highlights
+//https://read.amazon.com/notebook?asin=B01N5AX61W&contentLimitState=&
+
+//call - https://read.amazon.com/notebook?asin=(this).attr("id")&contentLimitState=&
+
+//const res = await fetch('https://read.amazon.com/notebook?asin=(this).attr("id")&contentLimitState=&');
+//console.log(res);
+//data = await res.text;
+//console.log(data);
+
+
+
+
+
+//important data
+//on ---> https://read.amazon.com/notebook?asin=(this).attr("id")&contentLimitState=&
+//id of h3 class to get book name -> a-spacing-top-small a-color-base kp-notebook-selectable kp-notebook-metadata
+
+
+///highlight data
+
+//<span id="annotationNoteHeader" class="a-size-small a-color-secondary aok-hidden kp-notebook-selectable 
+//kp-notebook-metadata">Note | Location:&nbsp;150</span>
